cf53b54888283b1a3073b98aa9c0bcef0913a49d
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-10-28 18:54:04 -0400

    Added counters for all attempted and completed

--- screens/ScoutingSheet.js
+++ screens/ScoutingSheet.js
@@ -1,6 +1,6 @@
-import { Linking, StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, StatusBar, Dimensions, SafeAreaView, TextInput } from 'react-native';
+import { Linking, StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, StatusBar, Dimensions, SafeAreaView, TextInput, Button } from 'react-native';
 import React, { Component } from "react"
-import { RadioButton } from 'react-native-paper';
+import { RadioButton, Divider, Checkbox } from 'react-native-paper';
 
 
 const width_proportion = Dimensions.get('window').width;
@@ -9,9 +9,22 @@ export default class ScoutingSheet extends Component {
     constructor(props) {
         super(props);
         this.state = {
+
             teamNumber: 0,
             matchNumber: 0,
-            color: ''
+            color: '',
+            crossTarmac: 0,
+
+            AutonuppHubAtt: 0,
+            AutonuppHubComp: 0,
+            AutonlowerHubAtt: 0,
+            AutonlowerHubComp: 0,
+
+            TeleopuppHubAtt: 0,
+            TeleopuppHubComp: 0,
+            TeleoplowerHubAtt: 0,
+            TeleoplowerHubComp: 0
+
         }
     }
 
@@ -33,6 +46,7 @@ export default class ScoutingSheet extends Component {
                 <View style={styles.MainContent}>
                     <ScrollView>
                         <View style={{ flexDirection: 'column' }}>
+
                             <Text style={{ fontSize: 22 }}>Team #</Text>
                             <View style={styles.InputView}>
                                 <TextInput
@@ -74,7 +88,128 @@ export default class ScoutingSheet extends Component {
                                     />
                                     <Text style={{ fontSize: 20, color: 'blue' }}>Blue</Text>
                                 </View>
+                            </View>
 
+
+                            {/* AUTON STARTS HERE*/}
+
+
+                            <Text style={{ fontSize: 25, marginTop: 10 }}>Auton</Text>
+                            <Divider style={{ marginTop: 5 }} />
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={styles.ArgText}>Cross Tarmac</Text>
+                                <View style={{ marginTop: 6, marginLeft: 5 }}>
+                                    <Checkbox.Android
+                                        status={this.state.crossTarmac == 1 ? 'checked' : 'unchecked'}
+                                        onPress={() => this.state.crossTarmac == 1 ? this.setState({ crossTarmac: 0 }) : this.setState({ crossTarmac: 1 })}
+                                        style={{ marginTop: 100 }}
+                                    />
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Upper Attempted</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonuppHubAtt: this.state.AutonuppHubAtt - 1 }) }}>
+                                            <Text style={styles.ArgText}>-</Text>
+                                        </TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.AutonuppHubAtt}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonuppHubAtt: this.state.AutonuppHubAtt + 1 }) }}>
+                                            <Text style={styles.ArgText}>+</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Upper Completed</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonuppHubComp: this.state.AutonuppHubComp - 1 }) }}>
+                                            <Text style={styles.ArgText}>-</Text>
+                                        </TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.AutonuppHubComp}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonuppHubComp: this.state.AutonuppHubComp + 1 }) }}>
+                                            <Text style={styles.ArgText}>+</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Lower Attempted</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonlowerHubAtt: this.state.AutonlowerHubAtt - 1 }) }}><Text style={styles.ArgText}>-</Text></TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.AutonlowerHubAtt}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonlowerHubAtt: this.state.AutonlowerHubAtt + 1 }) }}><Text style={styles.ArgText}>+</Text></TouchableOpacity>
+                                    </View>
+                                </View>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Lower Completed</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonlowerHubComp: this.state.AutonlowerHubComp - 1 }) }}><Text style={styles.ArgText}>-</Text></TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.AutonlowerHubComp}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ AutonlowerHubComp: this.state.AutonlowerHubComp + 1 }) }}><Text style={styles.ArgText}>+</Text></TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+
+
+                            {/* TELEOP STARTS HERE*/}
+
+
+                            <Text style={{ fontSize: 25, marginTop: 10 }}>Teleop</Text>
+                            <Divider style={{ marginTop: 5 }} />
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Upper Attempted</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleopuppHubAtt: this.state.TeleopuppHubAtt - 1 }) }}>
+                                            <Text style={styles.ArgText}>-</Text>
+                                        </TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.TeleopuppHubAtt}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleopuppHubAtt: this.state.TeleopuppHubAtt + 1 }) }}>
+                                            <Text style={styles.ArgText}>+</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Upper Completed</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleopuppHubComp: this.state.TeleopuppHubComp - 1 }) }}>
+                                            <Text style={styles.ArgText}>-</Text>
+                                        </TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.TeleopuppHubComp}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleopuppHubComp: this.state.TeleopuppHubComp + 1 }) }}>
+                                            <Text style={styles.ArgText}>+</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Lower Attempted</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleoplowerHubAtt: this.state.TeleoplowerHubAtt - 1 }) }}><Text style={styles.ArgText}>-</Text></TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.TeleoplowerHubAtt}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleoplowerHubAtt: this.state.TeleoplowerHubAtt + 1 }) }}><Text style={styles.ArgText}>+</Text></TouchableOpacity>
+                                    </View>
+                                </View>
+
+                                <View style={{ justifyContent: 'flex-end' }}>
+                                    <Text style={{ fontSize: 20 }}>Lower Completed</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleoplowerHubComp: this.state.TeleoplowerHubComp - 1 }) }}><Text style={styles.ArgText}>-</Text></TouchableOpacity>
+                                        <Text style={styles.ArgText}>{this.state.TeleoplowerHubComp}</Text>
+                                        <TouchableOpacity onPress={() => { this.setState({ TeleoplowerHubComp: this.state.TeleoplowerHubComp + 1 }) }}><Text style={styles.ArgText}>+</Text></TouchableOpacity>
+                                    </View>
+                                </View>
                             </View>
                         </View>
                     </ScrollView>
