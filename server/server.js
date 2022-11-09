920acdf20a270cb32bd749e024e86c27765a597f
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-11-09 08:36:11 -0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -30,4 +30,4 @@ app.post("/", async (req, res) => {
 })
 
 
-app.listen(3000, () => { console.log("Listening on 3000") })
+app.listen(process.env.PORT || 3000, () => { console.log("Listening on 3000") })
